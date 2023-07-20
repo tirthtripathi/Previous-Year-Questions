8d2089b4f6f88a316ca00168d2c07f5404eab92e
Tirth Tripathi <100989513+tirthtripathi@users.noreply.github.com>
2023-07-21 00:32:20 +0530

    added index.html

---
+++ javascript.js
@@ -0,0 +1,124 @@
+document.getElementById("Searchbutton").onclick = function(){
+    branch = document.getElementById("Branch").value;
+    
+    sem = document.getElementById("Sem").value;
+
+    if(branch == 'b1'){
+        switch(sem){
+            case 's1':
+                window.open("https://drive.google.com/drive/folders/1zmjdcFWXBP4ZLqt-VuSdfEQ3EjrwSAGE");
+                break;
+            case 's2':
+                window.open("https://drive.google.com/drive/folders/1Oltl-qgbl99g0NsHwaDm85Gul9Tno0VW");
+                break;
+            case 's3':
+                window.open("https://drive.google.com/drive/folders/1aTlnASVngPP4o1NLdl0TvGmmkeiPNp4L");
+                break;
+            case 's4':
+                window.open("https://drive.google.com/drive/folders/1PH9sEBLkT1zySrVTtaEB9iaaSyTZP_q3");
+                break;
+            case 's5':
+                window.open("https://drive.google.com/drive/folders/1qz1w0kAM1uP-ehRxy5_4sqDiKwe8K6H1");
+                break;
+            case 's6':
+                window.open("https://drive.google.com/drive/folders/1Dq1KNXjt_dxm7QrGPDpxajYqLR9_iSqh");
+                break;
+            case 's7':
+                window.open("https://drive.google.com/drive/folders/1tS5vNhrY-prk--gIAc2-KA2_PDKL8x_D");
+                break;
+            case 's8':
+                window.open("https://drive.google.com/drive/folders/118uk5-dG_dX_PDQTfjsb6BdPBAAFo2VF");
+                break;
+
+        }
+    }
+    else if(branch == 'b2'){
+        switch(sem){
+            case 's1':
+                window.open("https://drive.google.com/drive/folders/1DO6Xr52ZceSRcLT6-CwbnBXWThXiJ6le");
+                break;
+            case 's2':
+                window.open("https://drive.google.com/drive/folders/12bOhJ0E1Vth01IJ2Mj3BxssiNrW-hmmb");
+                break;
+            case 's3':
+                window.open("https://drive.google.com/drive/folders/1bB882A1Y99qYAVk1H4A1NndCbS_xZMK0");
+                break;
+            case 's4':
+                window.open("https://drive.google.com/drive/folders/1BjJe1VZPDomb36lawQIyXogQj8ciJuOd");
+                break;
+            case 's5':
+                window.open("https://drive.google.com/drive/folders/15x5QVZFv0PvUIN8o1UkYs1tUaJl3hqYr");
+                break;
+            case 's6':
+                window.open("https://drive.google.com/drive/folders/1Pg54Us7V7fYII1aPGoUx0KScmrYjGEj-");
+                break;
+            case 's7':
+                window.open("https://drive.google.com/drive/folders/1mOo7Nf56qlw9oQJp_UHDIsuC7BnbvZle");
+                break;
+            case 's8':
+                window.open("https://drive.google.com/drive/folders/1WBMn2xmdrZVANDPJFg8YYYGt2ulw04UA");
+                break;
+
+        }
+    }
+    else if(branch == 'b3'){
+        switch(sem){
+            case 's1':
+                window.open("https://drive.google.com/drive/folders/1Pu1rMVU902Wyxx9-il9OvYIoOtYuIxe8");
+                break;
+            case 's2':
+                window.open("https://drive.google.com/drive/folders/1XcAVqEZMbFisL8R6AIgXoJG8ssCjPTVk");
+                break;
+            case 's3':
+                window.open("https://drive.google.com/drive/folders/1bdFp8MZmdNA9bLWQzox4hO6ACi6jIxhI");
+                break;
+            case 's4':
+                window.open("https://drive.google.com/drive/folders/1mODoXgIs-PtDo-5Vb_ora5vmhxM89UZr");
+                break;
+            case 's5':
+                window.open("https://drive.google.com/drive/folders/1oi2TZ2Qn2SyNdZwoIIo3G3we7PBsyhgW");
+                break;
+            case 's6':
+                window.open("https://drive.google.com/drive/folders/1eaA8RxcO96VVPDLDccU1h5rlZnuwSnjM");
+                break;
+            case 's7':
+                window.open("https://drive.google.com/drive/folders/1F9rxWnvdu44jK5BABVnVWCbzMmeVRHhe");
+                break;
+            case 's8':
+                window.open("https://drive.google.com/drive/folders/1y-6nYJPAn_0doza3mhxhwPStweiJGRhq");
+                break;
+        }
+    }
+    else if(branch == 'b4'){
+        switch(sem){
+            case 's1':
+                window.open("https://drive.google.com/drive/folders/1wD6QIaIL191I2Xc7C4SovUGIm7BJ6sbT");
+                break;
+            case 's2':
+                window.open("https://drive.google.com/drive/folders/1nVOf1_xcL7g-chWZq4pU3bau8ZLPtD8j");
+                break;
+            case 's3':
+                window.open("https://drive.google.com/drive/folders/1GBOL-79_GK73Fz6qokXwpQgC9PX6B4TK");
+                break;
+            case 's4':
+                window.open("https://drive.google.com/drive/folders/1PuEnu3YcN9XGg8K_-X6TCkZwhvNIbX0X");
+                break;
+            case 's5':
+                window.open("https://drive.google.com/drive/folders/1443EeRj1iwIACOr9MxPBNUlRxug_afF2");
+                break;
+            case 's6':
+                window.open("https://drive.google.com/drive/folders/1MvUS9fxuIrF85rai7EEMahe2uDSm8fnY");
+                break;
+            case 's7':
+                window.open("https://drive.google.com/drive/folders/1qBo0Wlvx9LWiM9ltsjtCnIAzhVU-s6Da");
+                break;
+            case 's8':
+                window.open("https://drive.google.com/drive/folders/1WbJN8PJQsMz7i4RTQOUtyOwL0eyiLEUY");
+                break;
+        }
+    }
+
+    else{
+        alert("please selet your 'Department' and 'Sem'");
+    }
+}
